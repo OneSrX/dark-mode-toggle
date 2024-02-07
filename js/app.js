d0dd621302bec339d976312375a470e64136de52
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2024-02-07 10:51:56 +0100

    refactor dark mode

--- js/app.js
+++ js/app.js
@@ -1,27 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   const darkModeToggle = document.getElementById("dark-mode-toggle");
 
-  // Check if there's a saved dark mode preference
-  const isDarkMode = localStorage.getItem("darkMode") === "true";
-
-  // Set the initial state of the toggle based on the saved preference
-  darkModeToggle.checked = isDarkMode;
-
-  // Apply dark mode based on the saved preference
-  if (isDarkMode) {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
+  // Set initial dark mode state
+  setDarkMode(localStorage.getItem("darkMode") === "true");
 
   // Toggle dark mode on checkbox change
-  darkModeToggle.addEventListener("change", function () {
-    if (darkModeToggle.checked) {
-      enableDarkMode();
-    } else {
-      disableDarkMode();
-    }
-  });
+  darkModeToggle.addEventListener("change", toggleDarkMode);
+
+  // Function to set dark mode
+  function setDarkMode(isDarkMode) {
+    darkModeToggle.checked = isDarkMode;
+    if (isDarkMode) enableDarkMode();
+    else disableDarkMode();
+  }
+
+  // Function to toggle dark mode
+  function toggleDarkMode() {
+    setDarkMode(darkModeToggle.checked);
+  }
 
   // Enable dark mode
   function enableDarkMode() {
